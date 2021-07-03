94e1d31c86aeb1d5c4b64dd89ae1c91aea240296
uneme <unemekenta@gmail.com>
2021-07-03 15:56:57 +0900

    topoage create vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+top-page
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopPage from './components/TopPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopPage
   }
 }
 </script>
---
+++ frontend/src/components/TopPage.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.main
+  h1 My Diary
+  .main__button
+    button Let's go
+  img(alt="logo" src="../assets/Notes-bro.svg")
+</template>
+
+<script>
+export default {
+  name: 'TopPage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+img {
+  width: 50rem;
+}
+</style>
