2cab1114afd8e92f9cce44c4d35b2fa7a9350b53
ho2ri2s <ho2ri2s.github@gmail.com>
2021-07-12 03:57:19 +0900

    add new page

--- frontend/src/components/DiariesPage.vue
+++ frontend/src/components/DiariesPage.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
 h1 Diaries
+.main__button
+  button(@click="onClickNewDiaryButton") New Diary
 </template>
 
 <script>
@@ -13,5 +15,22 @@ export default {
       console.log(response.data);
     })
   },
+  methods: {
+    onClickNewDiaryButton: function() {
+      this.$router.push('/diaries/new')
+    }
+  },
 }
 </script>
+
+<style>
+/* FIXME: 共通のCSSで定義したい */
+.main__button button {
+  color: #ffffff;
+  font-weight: bold;
+  background-color: #C7B379;
+  padding: 0.5rem 1.0rem;
+  margin: 2.0rem;
+  border-radius: 0.3rem;
+}
+</style>
---
+++ frontend/src/components/NewPage.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+h1 Create New Diary
+form(action="/diaries/new" method="POST")
+  dl
+    dt: label(for="title") Title
+    input(type="text", name="diary_title", id="title")
+    dt: label(for="content") Content
+    textarea(type="text", name="diary_content", id="content")
+    .main__button
+      button(type="submit") Create
+</template>
+
+<script>
+
+export default {
+  name: "New",
+}
+</script>
+<style>
+/* FIXME: 共通のCSSで定義したい */
+.main__button button {
+  color: #ffffff;
+  font-weight: bold;
+  background-color: #C7B379;
+  padding: 0.5rem 1.0rem;
+  margin: 2.0rem;
+  border-radius: 0.3rem;
+}
+
+form {
+  width: 40rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
