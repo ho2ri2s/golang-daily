0002f5deb0e6e961280471ba109e90a863b4e3e4
uneme <unemekenta@gmail.com>
2021-07-17 17:02:39 +0900

    一覧ページ修正

--- frontend/src/components/DiariesPage.vue
+++ frontend/src/components/DiariesPage.vue
@@ -1,20 +1,33 @@
 <template lang="pug">
-h1 Diaries
-  MainButton(value="New Diary", @click="onClickNewDiaryButton")
+.diaries
+  h1 Diaries
+  .diaries__list
+    .diaries__list__item(v-for="listItem in this.diaryList")
+      diary-item(:listItem="listItem")
+  .diaries__btn
+    MainButton(value="New Diary", @click="onClickNewDiaryButton")
 </template>
 
 <script>
 import axios from "axios";
 import MainButton from "./common/MainButton.vue";
+import DiaryItem from "./common/Diary.vue";
 
 export default {
   name: "Diaries",
   components: {
     MainButton,
+    DiaryItem,
+  },
+  data () {
+    return {
+      diaryList: [],
+    }
   },
   created() {
     axios.get("/diaries").then((response) => {
       console.log(response.data);
+      this.diaryList= response.data;
     });
   },
   methods: {
@@ -24,3 +37,17 @@ export default {
   },
 };
 </script>
+
+<style>
+.diaries__list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.diaries__list__item {
+  width: calc(16rem - 2rem);
+  margin: 1rem 1rem;
+  border: solid 0.1rem;
+  border-radius: 0.3rem;
+}
+</style>
