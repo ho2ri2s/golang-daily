92ef9554cc4d72ef5c7b1d18f9a4ddc594d1af7c
uneme <unemekenta@gmail.com>
2021-07-17 17:04:58 +0900

    リスト内アイテムのコンポーネント化

---
+++ frontend/src/components/common/Diary.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.diary__item
+  img(alt="logo", src="../../assets/Notes-bro.svg")
+  h2 {{listItem.title}}
+
+</template>
+
+<script>
+export default {
+  name: "DiaryItem",
+  props: {
+    listItem: { type: Object, required: false },
+  },
+};
+</script>
+
+<style>
+.diary__item img {
+  border-bottom: solid 0.1rem;
+}
+</style>
